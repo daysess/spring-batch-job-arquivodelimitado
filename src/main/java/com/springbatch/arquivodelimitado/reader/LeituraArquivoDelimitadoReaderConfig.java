abeb21e6f6adcca575206f60ba32eaa1d9721f09
Dayse Soares <daysedesign@gmail.com>
2020-06-25 22:07:59 -0300

    Projeto Spring batch - Job que ler arquivo que contem campos delimitados

---
+++ src/main/java/com/springbatch/arquivodelimitado/reader/LeituraArquivoDelimitadoReaderConfig.java
@@ -0,0 +1,16 @@
+package com.springbatch.arquivodelimitado.reader;
+
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.springbatch.arquivodelimitado.dominio.Cliente;
+
+@Configuration
+public class LeituraArquivoDelimitadoReaderConfig {
+	
+	@Bean
+	public FlatFileItemReader<Cliente> leituraArquivoDelimitadoReader() {
+		return null;
+    }
+}
